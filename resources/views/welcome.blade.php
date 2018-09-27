99bf1b16099667a0e887346c3376b9998e6f48b2
Shafira Ananda <afishafiraa@gmail.com>
2018-09-27 10:39:29 +0700

    second

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Hell-o
                 </div>
 
                 <div class="links">
